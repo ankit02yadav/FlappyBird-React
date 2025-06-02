6959f4cdfded8c0ee4b5e2dd8deb88623ceff70c
Ankit Yadav <yadavankit02062005@gmail.com>
2025-06-02 10:56:13 +0530

    Update BirdFly.cs

--- Assets/BirdFly.cs
+++ Assets/BirdFly.cs
@@ -11,7 +11,7 @@ public class BirdFly : MonoBehaviour
     {   
         logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<logicScript>();
     }   
-  
+    
     // Update is called once per frame
     void Update()
     {
